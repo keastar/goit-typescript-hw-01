0c7b66164c0344bb3f185525c146a093680760e4
keastar <keastar1986@gmail.com>
2023-11-13 18:38:40 +0200

    Done 1st HW TS!
    Done 1st HW TS!

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+import { concatenation } from "./concatenation";
+const button = document.querySelector("button");
+const input = document.querySelector("input");
+input.value = "HEY, everySoul!";
+if (button && input) {
+  button.addEventListener("click", () => {
+    concatenation(input.value, "PEACE TO ALL!");
+  });
+}
+//# sourceMappingURL=index.js.map
